5dba058ebfd059558d9434cefdc71bf2bdc00e0f
pravinjenkins <pravinnikure300@gmail.com>
2024-02-23 13:41:21 +0530

    Initial commit

---
+++ src/main/java/com/app/pravin/chartservice/SpringCloudChartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.pravin.chartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudChartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudChartServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/pravin/chartservice/SpringCloudChartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pravin.chartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudChartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
