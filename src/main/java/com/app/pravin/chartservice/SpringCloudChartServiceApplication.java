a082034456625e5ee25a426e53f371650b0b12fc
pravinjenkins <pravinnikure300@gmail.com>
2024-03-11 15:35:02 +0530

    adding port

--- src/main/java/com/app/pravin/chartservice/SpringCloudChartServiceApplication.java
+++ src/main/java/com/app/pravin/chartservice/SpringCloudChartServiceApplication.java
@@ -2,8 +2,11 @@ package com.app.pravin.chartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class SpringCloudChartServiceApplication {
 
 	public static void main(String[] args) {
