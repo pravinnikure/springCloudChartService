19da64cfda4adc8b8b24148bd12a968bb8766796
pravinjenkins <pravinnikure300@gmail.com>
2024-03-14 13:14:38 +0530

    Adding actuator for refresh scope

--- src/main/java/com/app/pravin/chartservice/SpringCloudChartServiceApplication.java
+++ src/main/java/com/app/pravin/chartservice/SpringCloudChartServiceApplication.java
@@ -3,10 +3,11 @@ package com.app.pravin.chartservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-
+@RefreshScope
 public class SpringCloudChartServiceApplication {
 
 	public static void main(String[] args) {
